781419827dff80ba82f5f6f9bb617618e2b29185
Vlad Tagintsev <fiend3d@ya.ru>
2026-03-04 20:50:27 +0300

    improve build script

--- build.ps1
+++ build.ps1
@@ -1,8 +1,10 @@
 $commit    = git rev-parse --short HEAD
 $buildTime = Get-Date -Format "dd.MM.yyyy HH:mm"
-# $version   = git describe --tags --always --dirty 2>$null
 $version   = git describe --tags --abbrev=0
+$dirty     = git status --porcelain
+
 if (-not $version) { $version = "dev" }
+if ($dirty) { $version += "-dirty" }
 
 $ldflags = @(
     "-X 'main.Version=$version'"
